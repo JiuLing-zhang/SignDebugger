4442346f401f3e15157a14d318725045e7573521
九零 <78294929+JiuLing-zhang@users.noreply.github.com>
2023-10-18 11:01:38 +0800

    新增 Url 解码

---
+++ src/SignDebugger.Page/CommandManage/UrlDecodeCommand.cs
@@ -0,0 +1,23 @@
+﻿using System.Web;
+
+namespace SignDebugger.Page.CommandManage;
+internal class UrlDecodeCommand : ICommand
+{
+    public string Title => "Url 解码";
+    private string _text;
+    private string _result = "";
+    public UrlDecodeCommand(string text)
+    {
+        _text = text;
+    }
+
+    public void Execute()
+    {
+        _result = HttpUtility.UrlDecode(_text);
+    }
+
+    public string GetResult()
+    {
+        return _result;
+    }
+}
