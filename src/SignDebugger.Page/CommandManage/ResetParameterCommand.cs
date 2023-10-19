a7f6b9708edf4c89be1bbf95ee387b4349456b72
九零 <78294929+JiuLing-zhang@users.noreply.github.com>
2023-10-19 21:21:10 +0800

    新增参数编辑功能

---
+++ src/SignDebugger.Page/CommandManage/ResetParameterCommand.cs
@@ -0,0 +1,22 @@
+﻿using JiuLing.CommonLibs.ExtensionMethods;
+
+namespace SignDebugger.Page.CommandManage;
+internal class ResetParameterCommand : ICommand
+{
+    public string Title => $"重置参数";
+    private List<KeyValueItem> _parameters;
+    private string _result = "";
+    public ResetParameterCommand(List<KeyValueItem> parameters)
+    {
+        _parameters = parameters;
+    }
+    public void Execute()
+    {
+        _result = string.Join('&', _parameters.Where(x => x.Key.Trim().IsNotEmpty()).Select(x => $"{x.Key}={x.Value}").ToArray());
+    }
+
+    public string GetResult()
+    {
+        return _result;
+    }
+}
---
+++ src/SignDebugger.Page/KeyValueItem.cs
@@ -0,0 +1,11 @@
+﻿namespace SignDebugger.Page;
+public class KeyValueItem
+{
+    public string Key { get; set; }
+    public string Value { get; set; }
+    public KeyValueItem(string key = "", string value = "")
+    {
+        Key = key;
+        Value = value;
+    }
+}
