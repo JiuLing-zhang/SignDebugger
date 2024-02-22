cdf2e6ef58ced5586d709e7137545d28f01a4395
九零 <78294929+JiuLing-zhang@users.noreply.github.com>
2024-02-22 20:17:20 +0800

    新增 Base64 解码

---
+++ src/SignDebugger.Page/CommandManage/Base64DecodingCommand.cs
@@ -0,0 +1,23 @@
+﻿using JiuLing.CommonLibs.Security;
+
+namespace SignDebugger.Page.CommandManage;
+internal class Base64DecodingCommand : ICommand
+{
+    public string Title => "Base64 解码";
+    private string _text;
+    private string _result = "";
+    public Base64DecodingCommand(string text)
+    {
+        _text = text;
+    }
+
+    public void Execute()
+    {
+        _result = Base64Utils.Base64ToString(_text);
+    }
+
+    public string GetResult()
+    {
+        return _result;
+    }
+}
